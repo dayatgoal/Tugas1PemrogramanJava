d9b5a127c922fee6bb0a2b47407fa28c737037a5
Dayat Computer <Dayat Computer@DayatComputer>
2016-11-04 16:50:11 +0700

    12141377 Integer.valueOf(JOptionPane.showInputDialog untuk menginputkan jumlah jenis segitiga pada program  System.out.print untuk menampilkan hasil jenis segitiga hasil proses perintah pada program   for di gunakan untuk perulangan dalam pemrograman java

--- src/tugas/program/segitiga/TugasProgramSegitiga.java
+++ src/tugas/program/segitiga/TugasProgramSegitiga.java
@@ -7,3 +7,11 @@ public class TugasProgramSegitiga {
     public static void main(String[] args) {
     
          int i, a;
+          int Nilai;
+        
+        Nilai =Integer.valueOf(JOptionPane.showInputDialog("Jumlah jenis Segitiga:"));
+        System.out.print("Hasil jenis Segitiga yang ditampilkan");
+        
+        for(i=0;i<=Nilai;i++) {
+            for(a=0; a<i; a++) {
+                System.out.print("^");
