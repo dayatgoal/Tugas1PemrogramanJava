590a30767f37f3427442e4554b9c4f9f10679314
Dayat Computer <Dayat Computer@DayatComputer>
2016-11-04 16:02:47 +0700

    12141377 javax.swing.JOptionPane  menunjukkan bahwa kelas JOptionPane berada di paket swing dan paket swing sendiri berada di paket javax. public class  berfungsi untuk mengakses program dari dalam dan luar program. public static void main(string[ ] args) adalah fungsi main dimana public pada bagian ini berarti bahwa metode-metode dapat dipanggil didalam dan juga diluar kelas. static sendiri berarti sama untuk seluruh instant dari kelas dan void sendiri berarti bahwa metode tidak akan mengirim apapun setelah selesainya. Fungsi main ini berisi argument yang diambil dari ruang eksekusi. int i, a;adalah tipe data untuk angka numerik yang tidak menggunakan koma, untuk tipe data 32 bit.

---
+++ src/tugas/program/segitiga/TugasProgramSegitiga.java
@@ -0,0 +1,9 @@
+package tugas.program.segitiga;
+
+import javax.swing.JOptionPane;
+public class TugasProgramSegitiga {
+
+    
+    public static void main(String[] args) {
+    
+         int i, a;
